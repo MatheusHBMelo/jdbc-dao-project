3e5b0457f7225a7e2d4f790b73c00e6a8f3fe28a
MatheusHBMelo <matheushbmelo@gmail.com>
2023-04-28 21:24:20 -0300

    Seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,4 +1,102 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serial;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    private Department department;
+
+    public Seller() {
+    }
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+        this.id = Objects.requireNonNull(id, "O ID não pode ser nulo!");
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public String toString(){
+        return "ID: " + id + " - Name: " + name + " - Email: " + email + " - Birth Date: " + sdf.format(birthDate) +
+                " - Base Salary: " + String.format("%.2f", baseSalary) + " - Department id: " + department.getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seller seller = (Seller) o;
+
+        return id.equals(seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
